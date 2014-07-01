722d1bfb33e96687d62836ef429d96ef2cd4a177
Juan Pablo <juampi92@gmail.com>
2014-07-01 00:30:13 -0300

    Events collection

---
+++ js/app/collections/match_events.js
@@ -0,0 +1,38 @@
+define(['backbone'],
+  function (Backbone){
+
+    return Backbone.Collection.extend({
+    constructor: function(home,away){
+      Backbone.Collection.apply(this);
+
+      this.add(this.append(home,away,-1));
+
+      this.lastTime = this.last.time;
+    },
+    append: function(home,away,from){
+      var buffer = [];
+
+      this._appendOnlyNew(buffer,home,'home',from);
+      this._appendOnlyNew(buffer,away,'away',from);
+
+      this.size = home.length + away.length;
+
+      return _.sortBy(buffer,'time');
+    },
+    update: function(home,away){
+      var _size = home.length + away.length;
+      if (this.size >= size ) return;
+
+      this.add(this.append(home,away,this.lastTime));
+      this.lastTime = this.last.time;
+    },
+    _appendOnlyNew: function(out,array,team,from){
+      for (var i = array.length - 1; i >= 0; i--) {
+        if ( array[i].time <= from ) return;
+        
+        array[i].team = team;
+        out.unshift(array[i]);
+      }
+    }
+  });
+});
--- js/app/models/match.js
+++ js/app/models/match.js
@@ -1,5 +1,5 @@
-define(['backbone','model/team_match'],
-  function (Backbone,TeamMatch){
+define(['backbone','collection/match_events'],
+  function (Backbone,MatchEvents){
 
   return Backbone.Model.extend({
     defaults: {
@@ -16,28 +16,23 @@ define(['backbone','model/team_match'],
       this.date = json.datetime;
       this.status = json.status;
 
-      this.home = new TeamMatch('home',json.home_team,json.home_team_events,teams.fetchTeam(json.home_team.code),this);
-      this.away = new TeamMatch('home',json.away_team,json.away_team_events,teams.fetchTeam(json.away_team.code),this);
+      this.home = {
+        team: teams.fetchTeam(json.home_team.code),
+        goals: json.home_team.goals,
+        winning: false
+      };
+      this.away = {
+        team: teams.fetchTeam(json.away_team.code),
+        goals: json.away_team.goals,
+        winning: false
+      };
+
+      this.events = new MatchEvents(json.home_team_events,json.away_team_events);
 
       this.winner = json.winner_code;
-
-      this.updateWinner();
-
-      // Events
-      this.on('change:winner',this.updateWinner);
     },
     hasEnded: function(){
       return ( this.status == 'completed');
-    },
-    updateWinner: function(){
-      if ( this.winner == this.home.get('team').get('code') ) {
-        this.home.set('winner',true);
-        this.away.set('winner',false);
-      } else if ( this.winner == this.away.get('team').get('code') ) {
-        this.away.set('winner',true);
-        this.home.set('winner',false);
-      }
-      console.log("Cambio el winner");
     },
     update: function(json){
       this.set('status',json.status);
